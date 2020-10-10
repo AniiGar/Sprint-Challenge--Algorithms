7517d6e84fdd3c1301b7fcf63b863d087bb0a725
Ani Ta <aniigarris@gmail.com>
2020-10-10 12:52:41 -0600

    Completed short answers and count_th with errors.

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,13 +2,13 @@
 
 ## Exercise I
 
-a)
+a) This snippet is O(log n) or O(n). I am leaning toward log n.
 
 
-b)
+b) This snippet is O(n2) because it is a nested loop. Every time this code is executed the runtime complexity is exponentiated. 
 
 
-c)
+c) This snippet is O(n) because bunnies is equivalent to n. It is being called n times before it reaches the base case. 
 
 ## Exercise II
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,30 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-def count_th(word):
+# def count_th(word):
+
+#     if not word:
+#         return 0
+#     elif word[0]==th:
+#         return 1+count_th(word[1:])
+#     else:
+#         return count_th(word[1:])
     
-    # TBC
+    # count = 0
+  
+    # for i in test_str: 
+    #     if i == 'e': 
+    #         count = count + 1
     
-    pass
+
+def check(string,ch):
+      if not string:
+        return 0
+      elif string[0]==ch:
+            return 1+check(string[1:],ch)
+      else:
+            return check(string[1:],ch)
+# string=raw_input("Enter string:")
+# ch=raw_input("Enter character to check:")
+print("Count is:")
+print(check('dnvlajdlch chch adlfljlajch', 'ch'))
